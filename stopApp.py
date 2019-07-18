ac6909e0582d8afc1e6f2c0a4e05ef804e960593
Matthew Brandeburg <matthewbrandeburg@datamachines.io>
2019-07-17 17:11:45 -0700

    returns json of bus stop information

--- stopApp.py
+++ stopApp.py
@@ -1,4 +1,13 @@
 import os
+import requests
 
 key = os.environ['wmataKey']
-print(key)
+# print(key)
+
+stopID = 1001195 #dummy stop ID
+URL = "https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID={}".format(stopID)
+HEADER = {'api_key': key} #dictionary with my api auth parameters (it wants my api token)
+
+r = requests.get(url=URL, params= HEADER)
+data = r.json()
+print(data)
